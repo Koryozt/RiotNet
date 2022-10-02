d66df5600e1212c2e006bdd0d7e55ac008fb0bf7
Gustavo Silva <gustavoasc2004@gmail.com>
2022-10-02 16:35:26 -0400

    Added valorant API implementation

---
+++ Core/API/Valorant API/Interfaces/IMatchVal.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using RiotNet.Core.Miscellaneous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet.Core.API.Valorant_API.Interfaces
+{
+	public interface IMatchVal
+	{
+		Task<JObject> GetMatch(string matchId);
+		Task<JObject> GetMatchList(string puuid);
+		Task<JObject> RecentMatches(Queue queue);
+	}
+}
