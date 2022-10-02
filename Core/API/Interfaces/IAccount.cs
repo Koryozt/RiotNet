11041118dbf98485903e948c43861a97d6075442
Gustavo Silva <gustavoasc2004@gmail.com>
2022-10-02 17:24:27 -0400

    Added account endpoint

--- Core/API/Interfaces/IAccount.cs
+++ Core/API/Interfaces/IAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace RiotNet.Core.API.Interfaces
 {
-	internal interface IRiot
+	public interface IAccount
 	{
+		Task<JObject> GetAccountByPUUID(string puuid);
+		Task<JObject> GetAccountByRiotID(string gameName, string tagLine);
+		Task<JObject> GetActiveShard(string game, string puuid);
 	}
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using RiotNet.Core.API.GamesAPI;
-using RiotNet.Core.API.GamesAPI.Interfaces;
+using RiotNet.Core.API.GamesAPI.Interfaces; 
 using RiotNet.Core.Connection;
 using RiotNet.Core.Miscellaneous;
 
@@ -15,7 +15,7 @@ namespace RiotNet
 	{
 		public static async Task Main(string[] args)
 		{
-            RiotNetAPI create = new ("RGAPI-abc97b0e-6e5b-4f8e-89cd-6b22528f6e3a", Platforms.la1, Region.AMERICAS);
+            RiotNetAPI create = new ("YOUR API KEY", Platforms.la1, Region.AMERICAS);
 			LoL league = new LoL();
 
 			JObject response = await league.GetSummonerByAccountName("kayle carupanera");
